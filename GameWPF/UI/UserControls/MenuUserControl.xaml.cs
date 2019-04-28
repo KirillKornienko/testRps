6880b4f4d4c3ae92a2fbb386dc086b3a5836ea15
Кирилл Корниенко <kirea9638@gmail.com>
2019-04-28 05:43:39 +0300

    Поправил гитигнор

--- GameWPF/UI/UserControls/MenuUserControl.xaml.cs
+++ GameWPF/UI/UserControls/MenuUserControl.xaml.cs
@@ -2,8 +2,6 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Imaging;
-using System.Windows.Media;
-
 using System.IO;
 using System.Collections.Generic;
 using System.Windows.Input;
